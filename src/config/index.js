8261b7b17720c3931b520d65aef703b89956dbfa
MDSkrki <somkerekidev@hotmail.com>
2022-09-17 17:23:19 +0200

    implemented fallback middlewares

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const { day00Router } = require("../day00/router");
+const error404 = require("../middlewares/error404");
+const responseHandler = require("../middlewares/responseHandler");
 const healthCheck = require("./healthcheck");
 const app = express();
 
@@ -9,5 +11,9 @@ app.use(express.json());
 app.use("/health", healthCheck);
 
 app.use("/day00", day00Router);
+
+// Fallback middlewares
+app.use(error404);
+app.use(responseHandler);
 
 module.exports = app;
