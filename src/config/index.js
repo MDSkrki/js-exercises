8a87ea70e1bdc7838dd95f992d02d88082e07bca
MDSkrki <somkerekidev@hotmail.com>
2022-09-17 10:45:10 +0200

    implemented healthcheck and router

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const { day00Router } = require("../day00/router");
+const healthCheck = require("./healthcheck");
 const app = express();
 
 // Middleware config
 app.use(express.json());
+app.use("/health", healthCheck);
+
+app.use("/day00", day00Router);
 
 module.exports = app;
