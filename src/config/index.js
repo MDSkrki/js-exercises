d7c30ea9bc2884db1879085a1a653d89a253cca2
MDSkrki <somkerekidev@hotmail.com>
2022-09-17 17:26:48 +0200

    created fun easter egg endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,17 @@ const { day00Router } = require("../day00/router");
 const error404 = require("../middlewares/error404");
 const responseHandler = require("../middlewares/responseHandler");
 const healthCheck = require("./healthcheck");
+const { teapot } = require("./http");
 const app = express();
 
 // Middleware config
 app.use(express.json());
 app.use("/health", healthCheck);
 
+// Easter Egg
+app.use("/teapot", teapot);
+
+// Exercise folders routing
 app.use("/day00", day00Router);
 
 // Fallback middlewares
